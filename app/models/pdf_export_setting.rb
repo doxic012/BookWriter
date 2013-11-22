bae2373fdac7504c4980f64bca35f3570238320e
npolkow <niclaspolkow@googlemail.com>
2013-11-22 10:42:47 +0100

    Validatoren initial implementiert.

--- app/models/pdf_export_setting.rb
+++ app/models/pdf_export_setting.rb
@@ -1,4 +1,13 @@
 class PdfExportSetting < ActiveRecord::Base
   attr_accessible :foot, :head, :insertFoot, :insertHead, :marginBottom, :marginLeft, :marginRight, :marginTop, :tableOfContent
   belongs_to :user
+
+  #TODO: Sinnvolle maximal Ränder definieren
+  validates :marginLeft, :marginRight, :presence => true, :numericality => {:greater_than => 0.5, :less_than => 10}
+  validates :marginTop, :marginBottom, :presence => true, :numericality => {:greater_than => 0.5, :less_than => 20}
+
+  #Eventuell brauchen wir hier keine Begrenzung wenn der PDF-Export automatisch Zeilen umbricht.
+  validates :foot,:presence => true, :if => :insertFoot?, :length => {:maximum => 20}
+  validates :head,:presence => true, :if => :insertHead?, :length => {:maximum => 20}
+
 end
